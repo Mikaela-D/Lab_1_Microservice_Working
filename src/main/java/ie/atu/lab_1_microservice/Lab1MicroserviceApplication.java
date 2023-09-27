8964f8a003d38240e8f6e3eccac601fe76ee89da
Mikaela-D <G00401392@atu.ie>
2023-09-27 14:24:05 +0100

    Created a new spring boot project

---
+++ src/main/java/ie/atu/lab_1_microservice/Lab1MicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_1_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1MicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1MicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab_1_microservice/Lab1MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_1_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1MicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
