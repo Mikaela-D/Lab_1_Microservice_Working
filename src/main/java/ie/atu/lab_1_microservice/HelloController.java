9c8b5525e72dba06aea148ea218677e54b535d19
Mikaela-D <G00401392@atu.ie>
2023-09-27 14:29:38 +0100

    Added code to the application

--- src/main/java/ie/atu/lab_1_microservice/HelloController.java
+++ src/main/java/ie/atu/lab_1_microservice/HelloController.java
@@ -1,4 +1,12 @@
 package ie.atu.lab_1_microservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
